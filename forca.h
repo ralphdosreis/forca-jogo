7656e3d7ec373d35f9fe9fd37ca323bb68f587cb
ralphdosreis <ralphdosreistorres@gmail.com>
2024-12-08 16:27:38 -0300

    first commit

---
+++ forca.h
@@ -0,0 +1,11 @@
+#define TAMANHO_PALAVRA 20
+
+void abertura();
+void chuta();
+int jachutou(char letra);
+int acertou();
+void desenhaforca();
+void escolhepalavra();
+int enforcou();
+int adicionapalavra();
+int chuteserrados();
